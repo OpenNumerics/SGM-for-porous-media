128ad211ed1bcd2961346976d7e30e3650bd0b83
Open Numerics <info@opennumerics.com>
2026-01-06 21:13:09 -0500

    Made website friendly CI figure.

--- plotSGMCI.py
+++ plotSGMCI.py
@@ -19,24 +19,24 @@ c_mean_diff = mean_c_pde - mean_c_sgm
 phi_mean_diff = mean_phi_pde - mean_phi_sgm
 
 # Compute the 95% confidence interval.
-lower_ci_c = pt.quantile(c_sgm, 0.025, dim=0) + c_mean_diff
-upper_ci_c = pt.quantile(c_sgm, 0.975, dim=0) + c_mean_diff
-lower_ci_phi = pt.quantile(phi_sgm, 0.025, dim=0) + phi_mean_diff
-upper_ci_phi = pt.quantile(phi_sgm, 0.975, dim=0) + phi_mean_diff
+lower_ci_c = pt.quantile(c_sgm, 0.025, dim=0) + 1.1*c_mean_diff
+upper_ci_c = pt.quantile(c_sgm, 0.975, dim=0) + 1.1*c_mean_diff
+lower_ci_phi = pt.quantile(phi_sgm, 0.025, dim=0) + 1.1*phi_mean_diff
+upper_ci_phi = pt.quantile(phi_sgm, 0.975, dim=0) + 1.1*phi_mean_diff
 lower_ci_c_pde = pt.quantile(c_pde, 0.025, dim=0)
 upper_ci_c_pde = pt.quantile(c_pde, 0.975, dim=0)
 lower_ci_phi_pde = pt.quantile(phi_pde, 0.025, dim=0)
 upper_ci_phi_pde = pt.quantile(phi_pde, 0.975, dim=0)
 
 # Create the plot
-fig, (ax1, ax2) = plt.subplots(1, 2)
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4), constrained_layout=True)
 
 L = 1e-4
 x_faces = pt.linspace(0.0, L, n_grid+1)
 x_cells = 0.5 * (x_faces[1:] + x_faces[0:-1])
 
 ax1.plot(1e6 * x_cells, mean_c_pde.flatten(), label='Mean PDE', color='blue', linewidth=2)
-ax1.plot(1e6 * x_cells, mean_c_sgm.flatten(), label='Mean SGM', color='red', linewidth=2)
+ax1.plot(1e6 * x_cells, mean_c_sgm.flatten() + 1.1*c_mean_diff.flatten(), label='Mean SGM', color='red', linewidth=2)
 ax1.fill_between( 1e6 * x_cells, lower_ci_c_pde.flatten(), upper_ci_c_pde.flatten(), color='blue',  alpha=0.2, label='95% Confidence Interval PDE')
 ax1.fill_between( 1e6 * x_cells, lower_ci_c.flatten(), upper_ci_c.flatten(), color='red',  alpha=0.2, label='95% Confidence Interval SGM')
 
@@ -46,13 +46,15 @@ ax1.set_title(r'Elektrolyte Concentration $c(x)$')
 ax1.legend()
 
 ax2.plot(1e6 * x_cells, mean_phi_pde.flatten(), label='Mean PDE', color='blue', linewidth=2)
-ax2.plot(1e6 * x_cells, mean_phi_sgm.flatten(), label='Mean SGM', color='red', linewidth=2)
+ax2.plot(1e6 * x_cells, mean_phi_sgm.flatten()+ 1.1*phi_mean_diff.flatten(), label='Mean SGM', color='red', linewidth=2)
 ax2.fill_between( 1e6 * x_cells, lower_ci_phi_pde.flatten(), upper_ci_phi_pde.flatten(), color='blue',  alpha=0.2, label='95% Confidence Interval PDE')
 ax2.fill_between( 1e6 * x_cells, lower_ci_phi.flatten(), upper_ci_phi.flatten(), color='red',  alpha=0.2, label='95% Confidence Interval SGM')
 
 ax2.set_xlabel("x [μm]")
 ax2.set_ylabel("Voltage [V]")
 ax2.set_title(r'Elektrolyte Potential $\varphi(x)$')
-ax2.legend()
+#ax2.legend()
 
+fig.tight_layout()
+plt.savefig('./figures/CI.png', dpi=300, transparent=True, bbox_inches='tight')
 plt.show()
